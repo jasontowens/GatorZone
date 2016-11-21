e70b71de0710c228e7cd1752d8279bc9112ab890
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-21 18:31:35 -0500

    Update ServerInterface.java

--- src/Server/ServerInterface.java
+++ src/Server/ServerInterface.java
@@ -1,24 +1,47 @@
+class NotReceivedYetException extends Exception {
+        public NotReceivedYetException(String msg) {
+            super(msg);
+        }
+}
+
 public class ServerInterface {
     
     private static final int NUMBER_OF_TILES = 72;
+    Client client;
     String playerID;
     String opponentID;
+    String startingTile;
+    String tileStack;
     
     public ServerInterface() {
+        client = new Client();
+        client.startClient();
+        startingTile = "";
+        tileStack = "";
         playerID = "";
         opponentID = "";
     }
     
-    public Tile getStartingTile() {
-        Tile startTile = new Tile();
+    public Tile getStartingTile() throws NotReceivedYetException{
+        if (startingTile == "") {
+            throw new NotReceivedYetException("Server hasn't sent starting tile yet!");
+        } else {
         
+        Tile startTile = new Tile();
         return startTile;
+        
+        }
     }
     
-    public Tile[] getTileStack() {
-        Tile[] tiles = new Tile[NUMBER_OF_TILES];
+    public Tile[] getTileStack() throws NotReceivedYetException{
+        if (tileStack == "") {
+            throw new NotReceivedYetException("Server hasn't sent tile stack yet!");
+        } else {
         
+        Tile[] tiles = new Tile[NUMBER_OF_TILES];
         return tiles;
+        
+        }
     }
     
     public void joinServer(String tournamentPassword, String username, String password) {
