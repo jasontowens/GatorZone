68954f70ae00dd8ddc5e31ed1bbf4681c225fd60
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-22 17:11:48 -0500

    Update Edge.java

--- src/Board/Edge.java
+++ src/Board/Edge.java
@@ -1,5 +1,29 @@
-package Board;
-
 public class Edge {
-
+	
+	//1 = Lake
+	//2 = Jungle
+	//3 = game-trail
+	//4 = den
+	
+	int edgeType;
+	boolean hasMeeple;
+	boolean hasDeer;
+	boolean hasBuff;
+	boolean hasBoar;
+	public Edge(int type){
+		hasMeeple = false;
+		edgeType = type;		
+	}
+	public void placeMeeple(){
+		hasMeeple = true;
+	}
+	public void removeMeeple(){
+		if (hasMeeple) {
+			hasMeeple = false;
+		}
+		else{
+			System.out.println("No Meeple to remove");
+		}
+	}	
+	
 }
