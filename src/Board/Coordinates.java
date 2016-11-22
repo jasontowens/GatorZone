eeeaea7454cf5223ca84effeb99162cf6e3e0fc7
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-22 18:24:20 -0500

    Update Coordinates.java

--- src/Board/Coordinates.java
+++ src/Board/Coordinates.java
@@ -1,35 +1,35 @@
-public class Coordinates {
-	int numTiles = 144;
+package Board;
 
-	public Tiles[][] boardSpace = new Tiles[numTiles][numTiles];
+public class Coordinates {
+	private static final int NUM_TILES = 144;
+
+	public BoardSpace[][] boardSpace = new BoardSpace[NUM_TILES][NUM_TILES];
 	public Coordinates(){
 		
 	}
-	public void addTile(int x, int y, Tiles tile){
-		boardSpace[x][y] = tile;
-		return;
-	}
-	public void addTile(Tiles tile){
-		boardSpace[tile.x][tile.y] = tile;
-		return;
-	}
+	public void addTile(int x, int y, Tile tile){
+                if(validMove(x, y, tile))
+                    boardSpace[x][y].addTile(tile);
+                else
+                    System.out.println("Invalid placement location");
+        }
 	
 	// Looks at every tile adjacent to input tile and location to see if it is valid. 
-	public boolean checkValidMove(int x, int y, Tiles tile){ 
-		if (boardSpace[x][y] != null){
+	public boolean validMove(int x, int y, Tile tile){ 
+		if (boardSpace[x][y].hasTile){
 			System.out.println("There is already a tile here");
 			return false;
 		}
-		if(x != 0 && boardSpace[x-1][y] != null && boardSpace[x-1][y].r.edgeType != tile.l.edgeType){
+		if(x != 0 && boardSpace[x-1][y].hasTile && boardSpace[x-1][y].tile.r.edgeType != tile.l.edgeType){
 			return false;
 		}
-		if(x != (numTiles-1) && boardSpace[x+1][y] != null && boardSpace[x+1][y].l.edgeType != tile.r.edgeType){
+		if(x != (NUM_TILES - 1) && boardSpace[x+1][y].hasTile && boardSpace[x+1][y].tile.l.edgeType != tile.r.edgeType){
 			return false;
 		}
-		if(y != 0 && boardSpace[x][y-1] != null && boardSpace[x][y-1].b.edgeType != tile.t.edgeType){
+		if(y != 0 && boardSpace[x][y-1].hasTile && boardSpace[x][y-1].tile.b.edgeType != tile.t.edgeType){
 			return false;
 		}
-		if(y != (numTiles-1) && boardSpace[x][y+1] != null && boardSpace[x][y+1].t.edgeType != tile.b.edgeType){
+		if(y != (NUM_TILES - 1) && boardSpace[x][y+1].hasTile && boardSpace[x][y+1].tile.t.edgeType != tile.b.edgeType){
 			return false;
 		}
 		return true;
@@ -37,9 +37,9 @@ public class Coordinates {
 	
 	public String toString(){
 	String a = "";
-	for (int i = 0; i < numTiles; i++){
-		for (int j = 0; j < numTiles; j++ ){
-			if (boardSpace[j][i]!=null){
+	for (int i = 0; i < NUM_TILES; i++){
+		for (int j = 0; j < NUM_TILES; j++ ){
+			if (boardSpace[j][i].hasTile != false){
 				a += "# ";
 			}
 			else{
