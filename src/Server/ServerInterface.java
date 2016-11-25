b858485a1c85c8be0ec09fe63dc55eb56d4b4040
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-25 15:24:54 -0500

    Update ServerInterface.java

--- src/Server/ServerInterface.java
+++ src/Server/ServerInterface.java
@@ -1,6 +1,6 @@
-package server;
+package Server;
 
-import board.*;
+import Board.Tile;
 
 class NotReceivedYetException extends Exception {
         public NotReceivedYetException(String msg) {
@@ -62,7 +62,73 @@ public class ServerInterface {
     
     }
     
+    public String tileToServerString(Tile tile) {
+        String result = "";
+        result += featureIntToString(tile.t.getEdge());
+        result += featureIntToString(tile.r.getEdge());
+        result += featureIntToString(tile.b.getEdge());
+        result += featureIntToString(tile.l.getEdge());
+        result += featureIntToString(tile.getMiddle());
+        return result;
+    }
+    
     public Tile serverStringToTile(String serverString){
+        
+        int top = featureCharToInt(serverString.charAt(0));
+        int right = featureCharToInt(serverString.charAt(1));
+        int bottom = featureCharToInt(serverString.charAt(2));
+        int left = featureCharToInt(serverString.charAt(3));
+        int middle = featureCharToInt(serverString.charAt(4));
+        
+        return new Tile(right, top, left, bottom, middle);     
+    }
+    
+    public String featureIntToString(int feature) {
+        if (feature == 2)
+            return "J";
+        if (feature == 1)
+            return "L";
+        if (feature == 3)
+            return "T";
+        if (feature == 0)
+            return "-";
+        if (feature == 4)
+            return "B";
+        if (feature == 5)
+            return "C";
+        if (feature == 6)
+            return "D";
+        if (feature == 7)
+            return "P";
+        if (feature == 8)
+            return "X";
+        
+       System.out.println("Invalid Feature Specified");
+        return "E";
+    }
+    
+    public int featureCharToInt(char feature){
+        if (feature == 'J')
+            return 2;
+        if (feature == 'L')
+            return 1;
+        if (feature == 'T')
+            return 3;
+        if (feature == '-')
+            return 0;
+        if (feature == 'B')
+            return 4;
+        if (feature == 'C')
+            return 5;
+        if (feature == 'D')
+            return 6;
+        if (feature == 'P')
+            return 7;
+        if (feature == 'X')
+            return 8;
+      
+        System.out.println("Invalid Feature Specified");
+        return -1;
         
     }
 }
