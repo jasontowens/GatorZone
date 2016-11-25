9700419c9d0afe0aee563cde230a9bea8307a9d2
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-25 15:16:41 -0500

    Update Edge.java

--- src/Board/Edge.java
+++ src/Board/Edge.java
@@ -6,25 +6,9 @@ public class Edge {
 	//3 = game-trail
 	
 	int edgeType;
-	boolean hasTiger;
-	boolean hasDeer;
-	boolean hasBuff;
-	boolean hasBoar;
         
 	public Edge(int type){
-		hasTiger = false;
 		edgeType = type;		
-	}
-	public void placeTiger(){
-		hasTiger = true;
-	}
-	public void removeTiger(){
-		if (hasTiger) {
-			hasTiger = false;
-		}
-		else{
-			System.out.println("No Tiger to Remove!");
-		}
 	}
         
         public int getEdge(){
