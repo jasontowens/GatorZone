7649a33e3f2c1c7d6cec1032dacd1f065870e379
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-25 15:22:23 -0500

    Update GameDirector.java

--- src/Core/GameDirector.java
+++ src/Core/GameDirector.java
@@ -1,9 +1,13 @@
-import java.util.Stack;
+package Core;
+
+import Board.Tile;
 
 /*
  * The purpose of GameDirector is facilitate high level game flow, such as turns, scoring, and player control 
  */
 public class GameDirector {
-	public Stack<Tile> deck;
 	
+        public void run(){
+            
+        }
 }
