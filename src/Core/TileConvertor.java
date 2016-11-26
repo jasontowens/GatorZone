3db015a2104bf0f977edd070e766f33f458b9d94
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-26 02:48:09 -0500

    Update TileConvertor.java

--- src/Core/TileConvertor.java
+++ src/Core/TileConvertor.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Core;
 
 import Board.Tile;
 
-/**
- *
- * @author rg_ra
- */
 public class TileConvertor {
         public static String tileToServerString(Tile tile) {
         String result = "";
