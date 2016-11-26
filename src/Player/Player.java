da58b4e2bc130092bff3a973899dd7dd1a2ae60e
randygbrooks <randygeraldbrooks2@gmail.com>
2016-11-26 17:22:21 -0500

    Update Player.java

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,3 +1,8 @@
+package Player;
+
+import Board.BoardSpace;
+import Board.Coordinates;
+import Board.Tile;
 import java.util.List;
 
 /*
@@ -6,11 +11,40 @@ import java.util.List;
  * @author Jason Owens
  */
 public abstract class Player {
-	
-	protected List<BoardSpace> getFreeBoardSpaces(){
-		//get from Game Director?
-	}
-	protected Tile getNextTile(){
-		//request Tile from GameDirector
-	}
+        private static final int NUM_OF_TIGERS = 7;
+        private static final int NUM_OF_CROCS = 2;
+    
+        private int tigers;
+        private int crocodiles;
+        
+        public Player(){
+            tigers = NUM_OF_TIGERS;
+            crocodiles = NUM_OF_CROCS;
+        }
+    
+        public int numberOfTigers(){
+            return tigers;
+        }
+        
+        public int numberOfCrocodiles() {
+            return crocodiles;
+        }
+
+        public void useTiger() {
+            tigers--;
+        }
+        
+        public void useCroc(){
+            crocodiles--;
+        }
+        
+        public void getBackTiger() {
+            tigers++;
+        }
+        
+        public void getBackCroc() {
+            crocodiles++;
+        }
+
+    
 }
